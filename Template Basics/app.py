2b3b26ccfe0a6c2ccb280a8f9aa54d711eb5c981
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-09 16:48:58 -0500

    updated the code

--- Template Basics/app.py
+++ Template Basics/app.py
@@ -7,11 +7,11 @@ def index():
     return render_template('index.html')
 
 def upperLevel(name):
-    return not(any(x.isupper() for x in name))
+    return any(x.isupper() for x in name)
 def lowerLevel(name):
-    return not(any(x.islower() for x in name))
+    return any(x.islower() for x in name)
 def numberEnd(name):
-    return not(name[-1].isdigit())
+    return name[-1].isdigit()
 
 @app.route('/report')
 def report():
@@ -19,12 +19,11 @@ def report():
     upper = upperLevel(userName)
     lower = lowerLevel(userName)
     numbr = numberEnd(userName)
-    if not(upper and lower and numbr):
+    if upper and lower and numbr:
         result = True
     else:
         result=False
-    print(result)
-    return render_template('report.html',result=result,upper=upper,lower=lower,number=numbr)
+    return render_template('report.html',result=result,upper=not(upper),lower=not(lower),number=not(numbr))
 
 if __name__ =='__main__':
     app.run(debug=True)
