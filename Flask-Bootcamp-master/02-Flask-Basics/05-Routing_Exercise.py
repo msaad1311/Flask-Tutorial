ddcfc5575eca4e55852ec6fc7aa377a0aadb8ea8
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-09 13:07:24 -0500

    completed the first flask basic exercise

--- Flask-Bootcamp-master/02-Flask-Basics/05-Routing_Exercise.py
+++ Flask-Bootcamp-master/02-Flask-Basics/05-Routing_Exercise.py
@@ -1,22 +1,30 @@
 # Set up your imports here!
 # import ...
+from flask import Flask
+
+app = Flask(__name__)
 
 
-@app.route('') # Fill this in!
+@app.route('/') # Fill this in!
 def index():
     # Welcome Page
     # Create a generic welcome page.
-    pass
+    return 'this is the main page'
 
-@app.route('') # Fill this in!
+def latinName(name):
+    if name[-1]!='y':
+        return name+'y'
+    else:
+        return name[:-1]+'iful'
+
+@app.route('/puppy_latin/<name>') # Fill this in!
 def puppylatin(name):
     # This function will take in the name passed
     # and then use "puppy-latin" to convert it!
-
+    return f'Hi {name}, your latin name is {latinName(name)}'
     # HINT: Use indexing and concatenation of strings
     # For Example: "hello"+" world" --> "hello world"
-    pass
 
 if __name__ == '__main__':
     # Fill me in!
-    pass
+    app.run(debug=True)
